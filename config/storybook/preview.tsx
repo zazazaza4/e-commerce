b85f443b0bf05ffbd54d296807d968d3ebb5b593
Yaroslav <6715414@gmail.com>
2024-01-15 01:36:27 +0200

    feat: add storybook and setup this

---
+++ config/storybook/preview.tsx
@@ -0,0 +1,26 @@
+import type { Preview } from '@storybook/react';
+
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { TranslationDecorator } from '../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
+
+const preview: Preview = {
+  decorators: [
+    TranslationDecorator,
+    StyleDecorator,
+    RouterDecorator,
+    ThemeDecorator,
+  ],
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
