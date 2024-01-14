080086fff1bc7dd030aa9b81379f1d69c019d8c6
Yaroslav <6715414@gmail.com>
2024-01-15 01:53:07 +0200

    feat: configure i18n for internationalization

---
+++ src/shared/config/i18n/i18n.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'ukr',
+    debug: __IS_DEV__,
+    cleanCode: true,
+    nonExplicitSupportedLngs: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    supportedLngs: ['en', 'ukr'],
+    load: 'languageOnly',
+  });
+
+export default i18n;
